6e7c487428a0bcd8f1bc4ea7184f6c57e00980ad
Chirs <65650400+ChrisOliver2345@users.noreply.github.com>
2024-04-18 02:02:41 +0800

    Add files via upload

--- runR2.py
+++ runR2.py
@@ -9,7 +9,6 @@ def get_mat_path(sc_path, pa_path, output_path):
     importr("GSEABase")
     importr("AUCell")
     importr("SingleCellExperiment")
-    importr("SNFtool")
     print('R package load!')
     r.source('./AucForPy2.R')
     r.AUC(sc_path, pa_path, output_path)
